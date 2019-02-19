50986408dd541026235986dcbe415d83fa0ab048
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-02-19 23:24:45 +0100

    Added hook code

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react'
+
+const getOrientation = () => window.screen.orientation.type
+
+const useScreenOrientation = () => {
+  const [orientation, setOrientation] = useState(getOrientation())
+
+  const updateOrientation = event => {
+    setOrientation(getOrientation())
+  }
+
+  useEffect(() => {
+    window.addEventListener('orientationchange', updateOrientation)
+    return () => {
+      window.removeEventListener('orientationchange', updateOrientation)
+    }
+  }, [])
+
+  return orientation
+}
+
+export default useScreenOrientation
